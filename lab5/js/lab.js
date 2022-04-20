98e24f58ad9db3491065e370314a4b4792cefed5
annaclairecu <acummins@ucsc.edu>
2022-04-20 10:30:33 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Anna and Shay
+ * Created:   04.20.2022
+ *
+ * (c) Copyright Anna and Shay.
+ **/
+
+ // Define variables\
+ make = "Dodge";
+ model = "Ram";
+ color = "Silver";
+ year = 2007;
+
+ // Calculate
+ age = 2022 - year;
+
+ // Output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>");
